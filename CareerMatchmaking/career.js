84731cbed27fbda0a607dc6da718e3292a7e5152
David <David@DESKTOP-FU54B1B>
2024-05-12 08:06:32 -0400

    did the views for the nav

--- CareerMatchmaking/career.js
+++ CareerMatchmaking/career.js
@@ -2,3 +2,18 @@ function startQuiz() {
     window.location.href = "quiz.html";
 }
 
+function aboutUs(){
+    window.location.href = "aboutus.html";
+}
+
+function view(){
+    window.location.href = "view.html";
+}
+
+function contact(){
+    window.location.href = "contact.html";
+}
+
+function services(){
+    window.location.href = "services.html";
+}
